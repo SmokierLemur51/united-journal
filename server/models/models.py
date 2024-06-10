cd75f3bae48fc6a48d898bc4c8d967902ca78c8a
SmokierLemur51 <ldl6147@gmail.com>
2024-06-09 22:48:43 -0400

    check

--- server/models/models.py
+++ server/models/models.py
@@ -1,7 +1,7 @@
 import datetime
 from typing import List
 
-from sqlalchemy import Boolean, DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy import Boolean, DateTime, Float, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
@@ -11,8 +11,8 @@ from flask_sqlalchemy import SQLAlchemy
 class Base(DeclarativeBase):
     pass
 
-db = SQLAlchemy(model_class=Base)
 
+db = SQLAlchemy(model_class=Base)
 
 
 class Vendor(Base):
@@ -26,11 +26,10 @@ class Vendor(Base):
     state: Mapped[str] = mapped_column(String(25))
     zip: Mapped[str] = mapped_column(String(10))
     # relationships
-    products: Mapped[List['Product']] = relationship(back_populates='vendor')   
+    products: Mapped[List['Product']] = relationship(back_populates='vendor')
 
     def __repr__(self) -> str:
         return "{}".format(self.vendor)
-
 
 
 class Customer(Base):
@@ -38,7 +37,7 @@ class Customer(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
-    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())    
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
     deleted_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
     company: Mapped[str] = mapped_column(String(120), unique=True)
     street: Mapped[str] = mapped_column(String(200), unique=True)
@@ -50,107 +49,94 @@ class Customer(Base):
     contacts: Mapped[List["CustomerContact"]] = relationship(back_populates="company")
     orders: Mapped[List["Order"]] = relationship(back_populates="customer")
 
-    def __repr__(self) -> None:
+    def __repr__(self) -> str:
         return "{}".format(self.company)
-
-
 
 
 class CustomerContact(Base):
     __tablename__ = "customer_contacts"
-    
+
     id: Mapped[int] = mapped_column(primary_key=True)
     product: Mapped[str] = mapped_column(String(80))
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
-    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())    
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
     deleted_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
     company_id: Mapped[int] = mapped_column(ForeignKey("customers.id"))
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     phone: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(100), nullable=True)
-    
+
     company: Mapped["Customer"] = relationship(back_populates="contacts")
 
     def __repr__(self) -> str:
         return "Name: {} Phone: ({})-{}-{}".format(self.name, self.phone[0:3], self.phone[3:6], self.phone[6:])
 
-    
-
 
 class ProductCategory(Base):
-    __tablename__ = "product_categories"
-    
+    __tablename__: str = "product_categories"
+
     id: Mapped[int] = mapped_column(primary_key=True)
     category: Mapped[str] = mapped_column(String(120), unique=True)
     info: Mapped[str] = mapped_column(String(500), nullable=True)
-    
+
     # relationships
     products: Mapped[List["Product"]] = relationship(back_populates='category')
     sub_categories: Mapped[List["ProductSubCategory"]] = relationship(back_populates="parent_category")
-    
 
     def __repr__(self) -> str:
         return "{}".format(self.category)
 
 
-
-
-
 class ProductSubCategory(Base):
-    __tablename__ = "product_sub_categories"
+    __tablename__: str = "product_sub_categories"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     parent_category_id: Mapped[int] = mapped_column(ForeignKey("product_categories.id"))
     sub_category: Mapped[str] = mapped_column(String(120), unique=True)
     info: Mapped[str] = mapped_column(String(500), nullable=True)
-    
+
     # relationships
     parent_category: Mapped["ProductCategory"] = relationship(back_populates="sub_categories")
-
 
     def __repr__(self) -> str:
         return "Sub Category: <{}>, Parent Category: <{}>".format(
             self.sub_category, self.parent_category.category)
 
 
-
 # This product will not be associated with a warehouse, Product.id will be referenced
 # in the InventoryItem class.
 class Product(Base):
     __tablename__ = "products"
-    
+
     id: Mapped[int] = mapped_column(primary_key=True)
     product: Mapped[str] = mapped_column(String(80))
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
-    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())    
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
     deleted_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
     category_id: Mapped[int] = mapped_column(ForeignKey('product_categories.id'))
-    sub_category_id: Mapped[int] = mapped_column(ForeignKey('product_sub_categories.id'))    
+    sub_category_id: Mapped[int] = mapped_column(ForeignKey('product_sub_categories.id'))
     vendor_id: Mapped[int] = mapped_column(ForeignKey('vendors.id'))
     product: Mapped[str] = mapped_column(String(120), unique=True)
     cost: Mapped[float] = mapped_column(Float, nullable=False, default=0.0)
-    
+
     # need to add sizing dimensions
 
     # relationships
     vendor: Mapped['Vendor'] = relationship(back_populates="products")
     category: Mapped['ProductCategory'] = relationship(back_populates='products')
     sub_category: Mapped['ProductCategory'] = relationship(back_populates='products')
-    
-
-
 
 
 class PricingBucket(Base):
     # More information in obsidian-united/pricing/buckets.md
     __tablename__ = "pricing_buckets"
-    
+
     id: Mapped[int] = mapped_column(primary_key=True)
     bucket: Mapped[str] = mapped_column(String(80), unique=True)
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
-    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())    
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
     deleted_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
-    margin_percent: Mapped[float] = mapped_column(Float, default=0.0) 
+    margin_percent: Mapped[float] = mapped_column(Float, default=0.0)
     dividend: Mapped[float] = mapped_column(Float, nullable=True, default=0.0)
 
     # relationships
@@ -160,12 +146,8 @@ class PricingBucket(Base):
     def __repr__(self) -> str:
         return "{}".format(self.bucket)
 
-
     def margin_dividend(self) -> None:
-        self.dividend = 1.00-(self.margin_percent/100)
-
-
-
+        self.dividend = 1.00 - (self.margin_percent / 100)
 
 
 class OrderType(Base):
@@ -182,26 +164,23 @@ class OrderType(Base):
         return "{}".format(self.order_type)
 
 
-
 class Order(Base):
     __tablename__ = "orders"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
-    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())    
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
     deleted_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
-    customer_id:  Mapped[int] = mapped_column(ForeignKey("customers.id"))
+    customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id"))
     order_type_id: Mapped[int] = mapped_column(ForeignKey("order_types.id"))
     backorder_iteration: Mapped[int] = mapped_column(Integer, nullable=True)
 
     customer: Mapped["Customer"] = relationship(back_populates="orders")
     order_type: Mapped["OrderType"] = relationship(back_populates="orders")
     order_items: Mapped[list["OrderItem"]] = relationship(back_populates="order")
-    
 
     def __repr__(self) -> None:
         return "Order-{}-{}".format(self.id, self.backorder_iteration)
-
 
 
 class OrderItem(Base):
@@ -215,14 +194,11 @@ class OrderItem(Base):
     # relationships
     order: Mapped["Order"] = relationship(back_populates="order_items")
 
-
     def __repr__(self) -> None:
         return "Order-{} Line: "
 
-
     def get_line_id(self) -> None:
         return
-
 
 
 class Warehouse(Base):
@@ -238,12 +214,11 @@ class Warehouse(Base):
 
     locations: Mapped[List["WarehouseLocations"]] = relationship(back_populates="warehouse")
 
-
     def __repr__(self) -> str:
         return "Warehouse -> {}".format(self.codename)
 
 
-class WarehouseLocation(Base): 
+class WarehouseLocation(Base):
     __tablename__ = "warehouse_locations"
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -252,10 +227,8 @@ class WarehouseLocation(Base):
 
     warehouse: Mapped["Warehouse"] = relationship(back_populates="locations")
 
-
     def __repr__(self) -> str:
         return "Location: {} Warehouse-ID: {}".format(self.location, self.warehouse_id)
-
 
 
 class InventoryItem(Base):
@@ -263,10 +236,34 @@ class InventoryItem(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
-    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())    
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
     deleted_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
     product_id: Mapped[int]
     reserved_status: Mapped[bool] = mapped_column(Boolean, default=False)
     reserved_on: Mapped[int] = mapped_column(ForeignKey("orders.id"))
 
     reserved_for: Mapped["Order"] = relationship(back_populates="")
+
+
+class TestInventoryItem(Base):
+    __tablename__ = "test_inventory_items"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
+    deleted_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'))
+    item: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    # Global Trade Identification Number
+    gtin: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    info: Mapped[str] = mapped_column(Text, nullable=True)
+
+    def __repr__(self) -> str:
+        return self.item
+
+    def update_reservations(self, **kwargs) -> None:
+        """
+
+        """
+        pass
+
--- server/models/order_models.py
+++
@@ -1,9 +0,0 @@
-""" File: models/order_models.py
-"""
-
-
-# This might serve better being in a branch models file, where it can 
-# be something that builds off of each branch. 
-
-# class CommonLookups(Base):
-    # Average quantity ordered per week
